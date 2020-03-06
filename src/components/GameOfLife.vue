74e5ba4e9e881ac3cf60fb771fcda8247b56dfd8
Pavel Žůrek <PavelZrk@gmail.com>
2020-03-06 22:34:27 +0100

    Full page Game Of Life

--- src/components/GameOfLife.vue
+++ src/components/GameOfLife.vue
@@ -7,8 +7,6 @@
     name: "GameOfLife",
     data() {
       return {
-        sizeX: 20,
-        sizeY: 20,
         cellSize: 5
       }
     },
@@ -16,20 +14,24 @@
       this.canvasElement = document.getElementById("canvas");
       this.canvas = this.canvasElement.getContext("2d");
 
-      this.resizeCanvasOnPage();
-      this.initData();
+      this.resizeOnPage();
+
+      this.draw();
+      setInterval(this.tick, 200);
     },
     methods: {
-      resizeCanvasOnPage() {
+      resizeOnPage() {
         this.canvasElement.width  = window.innerWidth;
         this.canvasElement.height = window.innerHeight;
-      },
-      initData() {
+
+        this.sizeX = Math.floor(this.canvasElement.width / this.cellSize);
+        this.sizeY = Math.floor(this.canvasElement.height / this.cellSize);
+
         this.data = [];
         for (let i = 0; i < this.sizeX; i++) {
           let row = [];
           for (let j = 0; j < this.sizeY; j++) {
-            row[j] = 0;
+            row[j] = Math.floor(Math.random() * 2);
           }
           this.data[i] = row;
         }
@@ -51,13 +53,63 @@
             }
           }
         }
+      },
+      tick () {
+        this.generation();
+        this.draw();
+      },
+      generation () {
+        let newData = [];
+
+        for (let i = 0; i < this.sizeX; i++) {
+          let newRow = [];
+          for (let j = 0; j < this.sizeY; j++) {
+            let liveNeighboursCount = this.numberOfLiveNeighbours(i, j);
+            newRow[j] = this.data[i][j];
+            if (this.data[i][j] === 1) {
+              if (liveNeighboursCount < 2 || liveNeighboursCount > 3) {
+                newRow[j] = 0;
+              }
+            } else {
+              if (liveNeighboursCount === 3) {
+                newRow[j] = 1;
+              }
+            }
+          }
+          newData[i] = newRow;
+        }
+
+        this.data = newData;
+      },
+      numberOfLiveNeighbours (row, column) {
+        return this.getCellValue(row - 1, column - 1) +
+                this.getCellValue(row - 1, column) +
+                this.getCellValue(row - 1, column + 1) +
+                this.getCellValue(row, column - 1) +
+                this.getCellValue(row, column + 1) +
+                this.getCellValue(row + 1, column - 1) +
+                this.getCellValue(row + 1, column) +
+                this.getCellValue(row + 1, column + 1);
+      },
+      getCellValue (row, column) {
+        if (row < 0) {
+          row = this.sizeX - 1;
+        }
+        if (row >= this.sizeX) {
+          row = 0;
+        }
+        if (column < 0) {
+          column = this.sizeY - 1;
+        }
+        if (column >= this.sizeY) {
+          column = 0;
+        }
+        return this.data[row][column];
       }
     }
   }
 </script>
 
 <style scoped>
-#canvas {
 
-}
 </style>
