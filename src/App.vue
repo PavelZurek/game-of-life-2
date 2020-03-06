1212d934effe087186438075e2f443ca7468e871
Pavel Žůrek <PavelZrk@gmail.com>
2020-03-06 21:30:57 +0100

    Game Of Life Component with canvas

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <GameOfLife />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameOfLife from "./components/GameOfLife";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameOfLife
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/GameOfLife.vue
@@ -0,0 +1,26 @@
+<template>
+  <canvas id="canvas"></canvas>
+</template>
+
+<script>
+  export default {
+    name: "GameOfLife",
+    mounted() {
+      this.canvasElement = document.getElementById("canvas");
+      this.canvas = this.canvasElement.getContext("2d");
+      this.resizeCanvasOnPage();
+    },
+    methods: {
+      resizeCanvasOnPage() {
+        this.canvasElement.width  = window.innerWidth;
+        this.canvasElement.height = window.innerHeight;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+#canvas {
+
+}
+</style>
